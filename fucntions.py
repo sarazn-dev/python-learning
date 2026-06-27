25ee8fd1ad8d1fc4e71d36b57eae3b61855bf0cb
sara <sara@ulipsu.com>
2026-06-27 18:18:16 +0530

    sum fun

--- fucntions.py
+++ fucntions.py
@@ -2,3 +2,13 @@ def greet(name):
     return f"Hello, {name}."
 
 print(greet("Sara"))
+
+
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
